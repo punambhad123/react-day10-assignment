8ed9c9dc708da789192f6468d12ff1f4fa8f8598
punambhad123 <punambhad123@gmail.com>
2023-03-23 13:58:59 +0530

    react day10 assignment done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Tictactoe from './Components/Tictactoe';
+import Scroller from './Components/Scroller';
+function App() {
+  return (
+    <div>
+   <Scroller/>
+   <Tictactoe/>
+   </div>
+   
+   
+  );
+}
+
+export default App;
---
+++ src/Components/Scroller.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './Scroller.css'
+
+
+export default function Scroller() {
+  return (
+    <div>
+        <h2>Know Your Mentors</h2>
+    <div className='main'>
+        
+      <div className='Profile'>
+        <img src="https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=is&k=20&c=tw9TuTigzhSlLA_b1Avy0X6GNF9ZFVvgTHIZ9i68Q0I="  height="50px" width="50px" alt="" />
+        <h3>Taylor Smith</h3>
+        <p>Product manager @ Google India</p>
+        <p className='Exp'>4 yrs Exp</p>
+      </div>
+      <div className='Profile'>
+        <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" height="50px" width="50px" alt="" />
+        <h3>Leslie Alexander</h3>
+        <p>Product manager @ Google India</p>
+        <p className='Exp'>4 yrs Exp</p>
+      </div>
+      <div className='Profile'>
+        <img src="https://media.istockphoto.com/id/1407759041/photo/confident-happy-beautiful-hispanic-student-girl-indoor-head-shot-portrait.jpg?s=612x612&w=is&k=20&c=Ek4l8KN-zYJfldW2GEs_cxqRwPakUJz-TAU2d8t5QQA=" height="50px" width="50px"  alt="" />
+        <h3>Ralph Edwards</h3>
+        <p>Product manager @ Google India</p>
+        <p className='Exp'>4 yrs Exp</p>
+      </div>
+      <div className='Profile'>
+        <img src="https://media.istockphoto.com/id/1368424494/photo/studio-portrait-of-a-cheerful-woman.jpg?s=1024x1024&w=is&k=20&c=9eszAhNKMRzMHVp4wlmFRak8YyH3rAU8re9HjKA6h3A="  height="50px" width="50px" alt="" />
+        <h3>Albert Flores</h3>
+        <p>Product manager @ Google India</p>
+        <p className='Exp'>4 yrs Exp</p>
+      </div>
+      <div className='Profile'>
+        <img src="https://media.istockphoto.com/id/1376080879/photo/cheerful-smiling-model-profile-side-view-brunette-beauty-woman-with-nude-make-up-smooth-skin.jpg?s=612x612&w=is&k=20&c=JgLPSEVuNaBwUHAopBkrY1Tk4MXSclAd6RxA-Mt1H1A=" height="50px" width="50px"  alt="" />
+        <h3>Kathrayn Murphy</h3>
+        <p>Product manager @ Google India</p>
+        <p className='Exp'>4 yrs Exp</p>
+      </div>
+      <div className='Profile'>
+        <img src="https://media.istockphoto.com/id/1318858332/photo/headshot-portrait-of-smiling-female-employee-posing-in-office.jpg?s=612x612&w=is&k=20&c=f0NR-g76WDrQo_Qa7x3gXOGmmH-CIBZ3Ud-rQFRMlks=" height="50px" width="50px"  alt="" />
+        <h3>Noah Arnold</h3>
+        <p>Product manager @ Google India</p>
+        <p className='Exp'>4 yrs Exp</p>
+      </div>
+      <div className='Profile'>
+        <img src="https://media.istockphoto.com/id/1463474533/photo/calling-from-rush-hour-streets.jpg?s=612x612&w=is&k=20&c=q1i1FVi7Dv_Ux1k8ViOLJWmx-sLiheNTHNoUC5Eei_4=" height="50px" width="50px"  alt="" />
+        <h3>Alyssa Healy</h3>
+        <p>Product manager @ Google India</p>
+        <p className='Exp'>4 yrs Exp</p>
+      </div>
+      </div>
+
+    </div>
+  )
+}
